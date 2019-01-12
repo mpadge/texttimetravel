6c401e26435bb6e1079495356ac5aefad6c44fa0
mpadge <mark.padgham@email.com>
2019-01-12 13:49:14 +0100

    add annual param to keyness

--- R/keyness.R
+++ R/keyness.R
@@ -11,6 +11,8 @@
 #' measures of assocation.
 #' @param remove_keyword If `TRUE`, remove the specified keyword from results,
 #' leaving only associations with that word not the word itself.
+#' @param annual Calculate `keyness` statistics on annual bases, rather than for
+#' entire corpus at once.
 #' @return A \pkg{quanteda} `keyness` object listing words (`features`) and
 #' associated keyness statistics.
 #' @export
@@ -26,7 +28,7 @@
 #' x <- ttt_keyness (tok, "politic*")
 #' head (x, n = 20)
 ttt_keyness <- function (x, word = "school", window = 10,
-                         remove_keyword = FALSE)
+                         remove_keyword = FALSE, annual = FALSE)
 {
     if (!methods::is (x, "tokens"))
     {
@@ -39,7 +41,25 @@ ttt_keyness <- function (x, word = "school", window = 10,
                   "please submit a corpus or tokens object")
     }
 
-    keyness_core (x, word, window)
+    res <- NULL
+    if (!annual)
+        res <- keyness_core (x, word, window)
+    else {
+        yearvar <- grep ("year", names (docvars (x)), ignore.case = TRUE)
+        years <- docvars (x) [[yearvar]]
+        res <- list ()
+        for (y in years)
+        {
+            xy <- quanteda::tokens_subset (x, years == y)
+            temp <- keyness_core (xy, word, window)
+            if (!is.null (temp))
+            {
+                res [[length (res) + 1]] <- temp
+                names (res) [length (res)] <- y
+            }
+        }
+    }
+    return (res)
 }
 
 keyness_core <- function (x, word, window)
@@ -51,9 +71,17 @@ keyness_core <- function (x, word, window)
                                              window = window) %>%
         quanteda::dfm ()
 
-    res <- quanteda::textstat_keyness (rbind (word_dfm, not_word_dfm),
-                                       seq_len (quanteda::ndoc (word_dfm)))
-    res$n_target_rel <- res$n_target / sum (word_dfm)
-    res$n_reference_rel <- res$n_reference / sum (not_word_dfm)
+    res <- NULL
+    if (ncol (word_dfm) > 0 & ncol (not_word_dfm) > 0)
+    {
+        # wordaround before current PR to quanteda is accepted:
+        z <- rbind (word_dfm, not_word_dfm)
+        if (quanteda::ndoc (z) == 2)
+            docnames (z) <- c ("test", "reference")
+        res <- quanteda::textstat_keyness (z,
+                                           seq_len (quanteda::ndoc (word_dfm)))
+        res$n_target_rel <- res$n_target / sum (word_dfm)
+        res$n_reference_rel <- res$n_reference / sum (not_word_dfm)
+    }
     return (res)
 }
