e55af0f085c65768fb1e6b00d98335c686e1d11f
mpadge <mark.padgham@email.com>
2019-01-13 01:08:04 +0100

    extend plot.keyness_annual

--- R/plot-methods.R
+++ R/plot-methods.R
@@ -6,15 +6,22 @@
 #' @export
 plot.keyness_annual <- function (x, ...)
 {
-    feature <- chi2 <- year <- NULL # supress no visible binding notes
+    res <- feature <- chi2 <- year <- NULL # supress no visible binding notes
     if (is.data.frame (x))
     {
-        dplyr::select (x, c (feature, chi2, year)) %>%
+        res <- dplyr::select (x, c (feature, chi2, year)) %>%
             dplyr::mutate (year = as.numeric (year)) %>%
             ggplot2::ggplot (ggplot2::aes (x = year,
-                                           y = chi2,
-                                           colour = feature)) +
-            #ggplot2::geom_line () +
-            ggplot2::geom_smooth (method = loess)
+                                                  y = chi2,
+                                                  colour = feature))
+        if (nrow (x) < 20) {
+            res <- res + ggplot2::geom_line () +
+                ggplot2::geom_point (size = 2)
+        } else {
+            res <- res + ggplot2::geom_smooth (method = loess) +
+                ggplot2::geom_point (size = 2)
+        }
+        print (res)
     }
+    invisible (res)
 }
